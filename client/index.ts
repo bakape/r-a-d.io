45a4d775a128d1ba76b6261e198e9fdccf8171e3
bakape <scorpid33@gmail.com>
2017-10-28 20:23:45 +0300

    Semicolons

--- client/index.ts
+++ client/index.ts
@@ -30,90 +30,90 @@ type Song = {
 
 (() => {
 	// Cache elements to modify
-	const nowPlaying = document.getElementById("now-playing")
-	const listeners = document.getElementById("listeners")
+	const nowPlaying = document.getElementById("now-playing");
+	const listeners = document.getElementById("listeners");
 	const progress = {
 		time: document.getElementById("time-progress"),
-		bar: document.getElementById("progress-bar") as HTMLProgressElement
-	}
+		bar: document.getElementById("progress-bar") as HTMLProgressElement,
+	};
 	const dj = {
 		image: document.getElementById("dj-image") as HTMLImageElement,
 		name: document.getElementById("dj-name"),
-	}
-		// const lastPlayed = document.getElementById("last-played")
-		// const queue = document.getElementById("queue")
+	};
+	// const lastPlayed = document.getElementById("last-played")
+	// const queue = document.getElementById("queue")
 
-		// Reduce default volume
-		; (document.getElementById("stream") as HTMLAudioElement).volume = 0.2;
+	// Reduce default volume
+	(document.getElementById("stream") as HTMLAudioElement).volume = 0.2;
 
 	// Last fetched API data
 	// TODO: Initial data should be extracted from prerendered page as JSON
-	let data: API = null
+	let data: API = null;
 
 	// So we can make the next fetch happen quicker, when the last song ends
 	let nextFetch = 0;
 
 	// Correction for time skew between API server and client
-	let skew = 0
+	let skew = 0;
 
 	function fetchData() {
 		const xhr = new XMLHttpRequest()
 		xhr.onload = () => {
 			if (xhr.status === 200) {
-				data = xhr.response.main
-				skew = now() - data.current
-				render()
+				data = xhr.response.main;
+				skew = now() - data.current;
+				render();
 			}
-			nextFetch = setTimeout(fetchData, 10000)
+			nextFetch = setTimeout(fetchData, 10000);
 		}
 		xhr.onerror = () =>
-			nextFetch = setTimeout(fetchData, 10000)
-		xhr.responseType = "json"
-		xhr.open("GET", "https://r-a-d.io/api")
-		xhr.send()
+			nextFetch = setTimeout(fetchData, 10000);
+		xhr.responseType = "json";
+		xhr.open("GET", "https://r-a-d.io/api");
+		xhr.send();
 	}
 
 	function render() {
 		nowPlaying.textContent = data.np;
-		listeners.textContent = `Listeners: ${data.listeners}`
-		dj.image.src = "https://r-a-d.io/api/dj-image/" + data.dj.djimage
-		dj.name.textContent = data.dj.djname
-		renderProgress()
+		listeners.textContent = `Listeners: ${data.listeners}`;
+		dj.image.src = "https://r-a-d.io/api/dj-image/" + data.dj.djimage;
+		dj.name.textContent = data.dj.djname;
+		renderProgress();
 	}
 
 	function renderProgress() {
 		// Not yet fetched
 		if (!data) {
-			return
+			return;
 		}
 
-		const delta = data.end_time - data.start_time
-		const prog = now() - skew - data.start_time
+		const delta = data.end_time - data.start_time;
+		const prog = now() - skew - data.start_time;
 		progress.time.textContent
-			= `${formatDuration(prog)} / ${formatDuration(delta)}`
-		progress.bar.value = prog / delta
+			= `${formatDuration(prog)} / ${formatDuration(delta)}`;
+		progress.bar.value = prog / delta;
 		if (prog > delta) { // Data is stale
-			clearTimeout(nextFetch)
-			nextFetch = 0
-			fetchData()
+			clearTimeout(nextFetch);
+			nextFetch = 0;
+			fetchData();
 		}
 	}
 
 	// Return current Unix time
 	function now(): number {
-		return Math.floor(Date.now() / 1000)
+		return Math.floor(Date.now() / 1000);
 	}
 
 	// Pad with zero, if needed
 	function pad(n: number): string {
-		return n < 10 ? `0${n}` : n.toString()
+		return n < 10 ? `0${n}` : n.toString();
 	}
 
 	// Format duration string, such as "03:55", from second number
 	function formatDuration(n: number): string {
-		return `${pad(Math.floor(n / 60))}:${pad(n % 60)}`
+		return `${pad(Math.floor(n / 60))}:${pad(n % 60)}`;
 	}
 
-	fetchData()
-	setInterval(renderProgress, 1000)
-})()
+	fetchData();
+	setInterval(renderProgress, 1000);
+})();
